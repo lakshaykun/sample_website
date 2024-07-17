5b8b017484dd4f6d752aea87b8b0ec847ab5a0f1
lakshaykun <lakshu1000@gmail.com>
2024-07-17 21:55:36 +0530

    added jobitem.html

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+Services = [
+    {
+        'id' : '1',
+        'name' : 'Prediction Model',
+        'price' : '$200',
+        'duration' : "3 Days"
+    },
+    {
+        'id' : '2',
+        'name' : 'Time Series Model',
+        'price' : '$500',
+        'duration' : "3 Days"
+    },
+    {
+        'id' : '3',
+        'name' : 'Chatbot',
+        'price' : '$700',
+        'duration' : "3 Days"
+    },
+    {
+        'id' : '4',
+        'name' : 'Web Development',
+        'price' : '$1000',
+        'duration' : "3 Days"
+    }
+]
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', jobs = Services)
+
+# API for services
+@app.route('/api/services')
+def list_services():
+    return jsonify(Services)
 
 if __name__ == '__main__':
     app.run(debug = True)
